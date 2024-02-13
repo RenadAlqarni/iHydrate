a55e817fa3379e1349e3c1a30c37be53646381bf
Taalariyadh <TaalaAlhasan@iCloud.com>
2024-02-13 15:27:30 +0300

    Initial Commit

---
+++ Hydrate Watch App/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate Watch App
+//
+//  Created by Taala on 03/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Hydrate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
